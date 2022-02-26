8393b15709849a4501991a4dcf7cf6f326e9ce86
Stuart Wilson <stuartwilson94@gmail.com>
2022-02-26 14:01:36 -0600

    passed the tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+let repString;
+const repeatString = function(string, num) {
+    repString = '';
+    if ( num < 0 ) {
+        return "ERROR";
+    }
 
+    for (let i = 0; i  < num; i++){
+        repString = string + repString;
+    }
+    return repString;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
