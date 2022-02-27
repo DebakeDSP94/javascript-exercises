585063d0ca8d2cc78591d07f963648482a181672
Stuart Wilson <stuartwilson94@gmail.com>
2022-02-27 11:23:54 -0600

    learned reduce.  passed all tests.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	let sum = num1 + num2;
+  return sum
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	let sub = num1 - num2;
+  return sub;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let total = 0;
+  return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.length
+    ? array.reduce((accumulator, nextItem) => accumulator * nextItem)
+    : 0;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	let expo = num1 ** num2;
+  return expo;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let total = 1;
+  for (let i = 1; i <= num; i++) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
