0a240e647cbc0efb7ac29fb5cba54c635193d4cc
Stuart Wilson <stuartwilson94@gmail.com>
2022-02-27 17:22:39 -0600

    passed tests eventually

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
 
+const palindromes = function (string) {
+ //   string = string.toLowerCase();
+ //   string.replace((".",',',"!","?"), "");
+ //   const strArray = [...string];
+ //   const revArray = strArray.reverse();
+ //   for (let i = 0; i < strArray.length; i++) {
+ //       if (strArray[i] !== revArray[i]) {
+ //           return false;
+ //       }  
+ //       else {return true};
+ //   }
+    processedString = string.toLowerCase().replace(/[^a-z]/g, "");
+    return (
+        processedString
+            .split("")
+            .reverse()
+            .join("") == processedString
+    );
 };
 
 // Do not edit below this line
