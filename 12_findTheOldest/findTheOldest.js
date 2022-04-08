c086ab8ab3c5dea4c9964a1c1b7bbed27c76ed33
stuart wilson <stuartwilson94@gmail.com>
2022-04-08 13:46:01 -0500

    find the oldest and caesar cipher exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, nextOldest) => {
+        if(nextOldest.hasOwnProperty('yearOfDeath')) {
+            const age = nextOldest.yearOfDeath - nextOldest.yearOfBirth;
+            nextOldest.age = age;
+        } else {
+            const age = new Date().getFullYear() - nextOldest.yearOfBirth;
+            nextOldest.age = age;
+        }
+        if (!oldest.hasOwnProperty('age')){
+            oldest = nextOldest;
+        } else if (nextOldest.age > oldest.age) {
+            oldest = nextOldest;
+        }
+        return oldest;
+    }, {});
+    
 };
 
 // Do not edit below this line
--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,43 @@
-const caesar = function() {
+const caesar = function (message, adj) {
+	const upper = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('');
+	const lower = 'abcdefghijklmnopqrstuvwxyz'.split('');
+	const upperKey = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('');
+	const lowerKey = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
+	if (adj > 0) {
+		for (i = 0; i < adj; i++) {
+			temp = upperKey.shift();
+			upperKey.push(temp);
+			temp2 = lowerKey.shift();
+			lowerKey.push(temp2);
+		}
+	} else if (adj < 0) {
+		adj = -adj;
+		for (i = 0; i < adj; i++) {
+			temp = upperKey.pop();
+			upperKey.unshift(temp);
+			temp2 = lowerKey.pop();
+			lowerKey.unshift(temp2);
+		}
+	}
+
+	const newString = [];
+	const len = message.length;
+	for (let i = 0; i < len; i++) {
+		if (message[i] === ' ' || message[i] === '!' || message[i] === ',') {
+			newString.push(message[i]);
+		} else if (message[i] === message[i].toUpperCase()) {
+			const index2 = upper.indexOf(message[i]);
+			newString.push(upperKey[index2]);
+		} else if (message[i] === message[i].toLowerCase()) {
+			const index = lower.indexOf(message[i]);
+			newString.push(lowerKey[index]);
+		}
+	}
+
+	const newMessage = newString.join('');
+	console.log(newMessage);
+	return newMessage;
 };
 
 // Do not edit below this line
